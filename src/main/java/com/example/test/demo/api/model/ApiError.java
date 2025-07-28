76058a5130777fc8ccd7c0f5f62b866593f8fe4d
Adam Zalewski <zalewskiadam220@gmail.com>
2025-07-28 16:41:20 +0200

    Added error handling for parsing data

--- src/main/java/com/example/test/demo/api/model/ApiError.java
+++ src/main/java/com/example/test/demo/api/model/ApiError.java
@@ -2,9 +2,11 @@ package com.example.test.demo.api.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiError {
     @Getter @Setter private int status;
     @Getter @Setter private String message;
--- src/main/java/com/example/test/demo/service/GitService.java
+++ src/main/java/com/example/test/demo/service/GitService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -22,14 +23,20 @@ import com.example.test.demo.api.model.GitUserRepo.GitBranch;
 public class GitService {
     private String githubApiBaseUrl = "https://api.github.com";
     private ResponseErrorHandler noErrorHandler = new ResponseErrorHandler() {
-        @Override public boolean hasError(org.springframework.http.client.ClientHttpResponse response) { return false;}
-        @Override public void handleError(org.springframework.http.client.ClientHttpResponse response) {}
+        @Override
+        public boolean hasError(org.springframework.http.client.ClientHttpResponse response) {
+            return false;
+        }
+
+        @Override
+        public void handleError(org.springframework.http.client.ClientHttpResponse response) {
+        }
     };
 
     public Object getNonForkedUserRepos(String username) {
         String reposUrl = githubApiBaseUrl + "/users/" + username + "/repos";
         ResponseEntity<String> response;
-        
+
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setErrorHandler(noErrorHandler);
 
@@ -41,36 +48,48 @@ public class GitService {
         response = restTemplate.exchange(reposUrl, HttpMethod.GET, entity, String.class);
 
         if (response.getStatusCode().value() != 200) {
-            return new ApiError(response.getStatusCode().value(), new JSONObject(response.getBody()).getString("message"));
+            ApiError error = new ApiError();
+            error.setStatus(response.getStatusCode().value());
+            try {
+                error.setMessage(new JSONObject(response.getBody()).getString("message"));
+            } catch (JSONException e) {
+                error.setMessage("No message");
+            }
+            return error;
         }
 
         JSONArray reposArray = new JSONArray(response.getBody());
         List<GitUserRepo> nonForkedRepos = new ArrayList<>();
 
         for (int i = 0; i < reposArray.length(); i++) {
-            JSONObject repo = reposArray.getJSONObject(i);
+            try {
+                JSONObject repo = reposArray.getJSONObject(i);
 
-            if (repo.getBoolean("fork"))
-                continue;
+                if (repo.getBoolean("fork"))
+                    continue;
 
-            GitUserRepo gitUserRepo = new GitUserRepo();
-            gitUserRepo.setRepoName(repo.getString("name"));
-            gitUserRepo.setLogin(repo.getJSONObject("owner").getString("login"));
+                GitUserRepo gitUserRepo = new GitUserRepo();
+                gitUserRepo.setRepoName(repo.getString("name"));
+                gitUserRepo.setLogin(repo.getJSONObject("owner").getString("login"));
 
-            ArrayList<GitBranch> branches = new ArrayList<>();
-            String branchesUrl = repo.getString("branches_url").replaceAll("\\{.*?\\}", "");
-            response = restTemplate.exchange(branchesUrl, HttpMethod.GET, entity, String.class);
-            JSONArray repoBranches = new JSONArray(response.getBody());
-            for (int j = 0; j < repoBranches.length(); j++) {
-                JSONObject repoBranch = repoBranches.getJSONObject(j);
-                GitBranch branch = gitUserRepo.new GitBranch(
-                        repoBranch.getString("name"),
-                        repoBranch.getJSONObject("commit").getString("sha"));
-                branches.add(branch);
+                ArrayList<GitBranch> branches = new ArrayList<>();
+                String branchesUrl = repo.getString("branches_url").replaceAll("\\{.*?\\}", "");
+                response = restTemplate.exchange(branchesUrl, HttpMethod.GET, entity, String.class);
+                JSONArray repoBranches = new JSONArray(response.getBody());
+                for (int j = 0; j < repoBranches.length(); j++) {
+                    JSONObject repoBranch = repoBranches.getJSONObject(j);
+                    GitBranch branch = gitUserRepo.new GitBranch(
+                            repoBranch.getString("name"),
+                            repoBranch.getJSONObject("commit").getString("sha"));
+                    branches.add(branch);
+                }
+
+                gitUserRepo.setBranches(branches); // You can populate branches if needed
+                nonForkedRepos.add(gitUserRepo);
+            } catch (JSONException e) {
+                return new ApiError(0, "Error while parsing API response");
             }
 
-            gitUserRepo.setBranches(branches); // You can populate branches if needed
-            nonForkedRepos.add(gitUserRepo);
         }
 
         return nonForkedRepos;
