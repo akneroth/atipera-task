60611c41e97f26dfb31d6bd696911b207d338445
Adam Zalewski <zalewskiadam220@gmail.com>
2025-07-28 15:58:29 +0200

    removed unused imports

--- src/main/java/com/example/test/demo/api/controller/ApiController.java
+++ src/main/java/com/example/test/demo/api/controller/ApiController.java
@@ -1,13 +1,10 @@
 package com.example.test.demo.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.test.demo.api.model.GitUserRepo;
 import com.example.test.demo.service.GitService;
 
 @RestController
